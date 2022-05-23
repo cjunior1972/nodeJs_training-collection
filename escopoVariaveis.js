fa99ce63f18ca02243847fa53a4b5052ca694684
Carlos Augusto <cjunior@hotmail.com>
2022-05-23 00:22:39 -0300

    atualizando e testando melhor escopo de variáveis

--- escopoVariaveis.js
+++ escopoVariaveis.js
@@ -1,11 +1,26 @@
 
 function imprimeNome(){
     if(true)     {
-        var nome = "Dani";
+        var nome = "Dani";  //var é um tipo de variavel que vale pra função inteira
     }
+    console.log(nome);
+
+
+    for (var numero = 0 ; numero < 3; numero ++) {
+        console.log(numero);
+    }
+    console.log(numero);
+}
+
+imprimeNome();
+
+
+
+
+function imprimeNome2() { 
+    let nome = "Dani 2";        //let é um tipo de variável apenas no bloco que ele faz parte
     console.log(nome);
 }
 
-
-imprimeNome();
+imprimeNome2();
 
